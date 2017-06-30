5663d184ae9966a2294527e8bcf0311b888b9e0c
김태인 <terry@gimtaein-ui-MacBook-Pro.local>
2017-06-30 15:21:45 +0900

    initial commit

---
+++ SSUMAP_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SSUMAP_iOS
+//
+//  Created by 김태인 on 2017. 6. 30..
+//  Copyright © 2017년 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
