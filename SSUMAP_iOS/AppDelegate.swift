e8e7ba0da68c2ea383e8fdf2daee50414555a981
김태인 <terry@gimtaein-ui-MacBook-Pro.local>
2017-07-01 14:08:40 +0900

    Splash, GoogleMap Integeration Finished

--- SSUMAP_iOS/AppDelegate.swift
+++ SSUMAP_iOS/AppDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017년 Personal. All rights reserved.
 //
 
+import GoogleMaps
 import UIKit
 
 @UIApplicationMain
@@ -16,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyDYjKXqKl0iUQAfUXscUfD9D3j0WtPX0hM")
+        UINavigationBar.appearance().barStyle = .blackOpaque
+        
         return true
     }
 
